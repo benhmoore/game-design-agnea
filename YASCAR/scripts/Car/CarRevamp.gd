2d11b279c90d81444428faa0e0f695c8196886ca
Ben Moore <ben@benhmoore.com>
2023-04-23 11:44:08 -0500

    Added split-screen. Added support for multiple cars with customizable colors.

--- YASCAR/scripts/Car/CarRevamp.gd
+++ YASCAR/scripts/Car/CarRevamp.gd
@@ -2,6 +2,10 @@ extends VehicleBody3D
 
 enum ControlScheme { ARROWS, WASD }
 
+# Car color
+@export var car_color: Color = Color(1, 0, 0)
+@onready var car_mesh: MeshInstance3D = $CarBody/body
+
 # Car camera
 @onready var camera: Camera3D = $Camera3D
 
@@ -16,11 +20,22 @@ enum ControlScheme { ARROWS, WASD }
 
 # Minimum and maximum FOV for the camera
 @export var min_fov = 70.0
-@export var max_fov = 120.0
+@export var max_fov = 110.0
 
 # Front and rear wheels
 @onready var front_wheels = [get_node("FrontLeftWheel"), get_node("FrontRightWheel")]
 @onready var rear_wheels = [get_node("RearLeftWheel"), get_node("RearRightWheel")]
+
+func _ready():
+	set_car_color(car_color)  # Set the car's color to red
+
+func set_car_color(color: Color):
+	var newMaterial = StandardMaterial3D.new() 
+	newMaterial.albedo_color = color
+	car_mesh.set_surface_override_material(1, newMaterial)
+	
+#	car_mesh.mesh.material.albedo_color = Color(0,1,0)
+	
 
 func _physics_process(delta):
 	# Get input values for acceleration, braking, and steering
@@ -41,6 +56,7 @@ func _physics_process(delta):
 	# Update camera FOV based on car's velocity
 	update_camera_fov(delta)
 
+
 func limit_top_speed():
 	if linear_velocity.length() > max_speed:
 		linear_velocity = linear_velocity.normalized() * max_speed
@@ -59,7 +75,5 @@ func get_steering_input() -> float:
 
 func update_camera_fov(delta):
 	var speed_ratio = linear_velocity.length() / max_speed
-	print(speed_ratio)
 	var target_fov = lerp(min_fov, max_fov, speed_ratio)
 	camera.fov = lerp(camera.fov, target_fov, 5 * delta)
-	print(camera.fov)
