75dd900eb5db41a118127099103069be69363bb7
Ben Moore <ben@benhmoore.com>
2023-04-23 10:45:51 -0500

    Improved feel of movement. Added support for specifying control scheme.

--- YASCAR/scripts/Car/CarRevamp.gd
+++ YASCAR/scripts/Car/CarRevamp.gd
@@ -1,36 +1,65 @@
 extends VehicleBody3D
 
-# The acceleration force applied to the vehicle when pressing the up arrow
-@export var acceleration = 250.0
+enum ControlScheme { ARROWS, WASD }
 
-# The brake force applied to the vehicle when pressing the down arrow
-@export var break_force = 100.0
+# Car camera
+@onready var camera: Camera3D = $Camera3D
 
-# The steering angle limit for the front wheels
-@export var steer_limit = deg_to_rad(25)
+# Control scheme selection
+@export var control_scheme:ControlScheme = ControlScheme.ARROWS
 
-# The top speed limit of the vehicle
-@export var top_speed = 50.0  # You can set your desired top speed here
+# Acceleration, braking, steering limit, and max speed parameters
+@export var acceleration_force = 250.0
+@export var braking_force = 100.0
+@export var steering_limit = deg_to_rad(25)
+@export var max_speed = 50.0
 
-# The front wheels that are used for steering
+# Minimum and maximum FOV for the camera
+@export var min_fov = 70.0
+@export var max_fov = 120.0
+
+# Front and rear wheels
 @onready var front_wheels = [get_node("FrontLeftWheel"), get_node("FrontRightWheel")]
-
-# The rear wheels that are used for traction
 @onready var rear_wheels = [get_node("RearLeftWheel"), get_node("RearRightWheel")]
 
 func _physics_process(delta):
-	# Get the input values for acceleration, brake and steering
-	var move_input = Input.get_action_strength("ui_up") - Input.get_action_strength("ui_down")
-	var turn_input = Input.get_action_strength("ui_left") - Input.get_action_strength("ui_right")
+	# Get input values for acceleration, braking, and steering
+	var accel_input = get_acceleration_input()
+	var steer_input = get_steering_input()
 
-	# Apply the engine force to the rear wheels
+	# Apply engine force to rear wheels
 	for wheel in rear_wheels:
-		wheel.engine_force = -move_input * acceleration
+		wheel.engine_force = -accel_input * acceleration_force
 
-	# Apply the steering angle to the front wheels
+	# Apply steering angle to front wheels
 	for wheel in front_wheels:
-		wheel.steering = turn_input * steer_limit
+		wheel.steering = steer_input * steering_limit
 
-	# Limit the vehicle's top speed
-	if linear_velocity.length() > top_speed:
-		linear_velocity = linear_velocity.normalized() * top_speed
+	# Limit vehicle's top speed
+	limit_top_speed()
+
+	# Update camera FOV based on car's velocity
+	update_camera_fov(delta)
+
+func limit_top_speed():
+	if linear_velocity.length() > max_speed:
+		linear_velocity = linear_velocity.normalized() * max_speed
+
+func get_acceleration_input() -> float:
+	if control_scheme == ControlScheme.ARROWS:
+		return Input.get_action_strength("ui_up") - Input.get_action_strength("ui_down")
+	else:
+		return Input.get_action_strength("move_forward") - Input.get_action_strength("move_backward")
+
+func get_steering_input() -> float:
+	if control_scheme == ControlScheme.ARROWS:
+		return Input.get_action_strength("ui_left") - Input.get_action_strength("ui_right")
+	else:
+		return Input.get_action_strength("move_left") - Input.get_action_strength("move_right")
+
+func update_camera_fov(delta):
+	var speed_ratio = linear_velocity.length() / max_speed
+	print(speed_ratio)
+	var target_fov = lerp(min_fov, max_fov, speed_ratio)
+	camera.fov = lerp(camera.fov, target_fov, 5 * delta)
+	print(camera.fov)
