7651c6879163a44c0b251c95bec038ec01d5027a
Ben Moore <ben@benhmoore.com>
2023-05-07 22:06:55 -0500

    Revert "Working minimal HUD"

--- YASCAR/HUD.gd
+++ YASCAR/HUD.gd
@@ -1,18 +1,12 @@
-extends CanvasLayer
+extends Node3D
 
-@export var checkpoint_controller: Node3D
+@onready var checkpoint_controller
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	
-	checkpoint_controller.connect("car_won", _on_car_won)
-	
 	pass # Replace with function body.
 
-func _on_car_won(car):
-	
-	print(car.name, " WON!")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
