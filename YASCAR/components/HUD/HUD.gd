1076a930401f6f2c8071ff16544faad739b2126a
Graham Sanders <git@grahamsanders.org>
2023-05-07 20:12:20 -0500

    Working debug messages

--- YASCAR/components/HUD/HUD.gd
+++ YASCAR/components/HUD/HUD.gd
@@ -4,16 +4,19 @@ extends CanvasLayer
 
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
-	
-	checkpoint_controller.connect("car_won", _on_car_won)
-	
+func _ready():	
 	pass # Replace with function body.
-
-func _on_car_won(car):
-	
-	print(car.name, " WON!")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_checkpoint_controller_car_lapped(car):
+	print(car.name, " lapped!")
+	pass # Replace with function body.
+
+
+func _on_checkpoint_controller_car_won(car):
+	print(car.name, " WON!")
+	pass # Replace with function body.
